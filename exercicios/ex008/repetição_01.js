0904d90c15914115c075b2a7e01a489ff1b8a388
Jairo Magno <113738210+JairoMagno@users.noreply.github.com>
2023-03-12 15:12:53 -0300

    repeticoes

---
+++ exercicios/ex008/repetição_01.js
@@ -0,0 +1,14 @@
+//Aprendendo repetições em JavaScript
+
+var c = 1
+var cont = 1
+
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c ++
+}
+
+do{
+    console.log(`Passo ${cont}`)
+    cont ++
+} while (cont <= 6)

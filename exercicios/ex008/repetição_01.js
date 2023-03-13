aef3a630f35da1b0b9e73210b1ee3a24782d4ca2
Jairo Magno <113738210+JairoMagno@users.noreply.github.com>
2023-03-13 06:23:29 -0300

    repetições em JS

--- exercicios/ex008/repetição_01.js
+++ exercicios/ex008/repetição_01.js
@@ -3,12 +3,21 @@
 var c = 1
 var cont = 1
 
+
 while (c <= 6) {
     console.log(`Passo ${c}`)
     c ++
 }
 
+//--------------------------------------------------
+
 do{
-    console.log(`Passo ${cont}`)
+    console.log(`Contagem ${cont}`)
     cont ++
 } while (cont <= 6)
+
+//--------------------------------------------------
+
+for (contador = 1; contador <= 6; contador++) {
+    console.log(`Tentativa ${contador}`)
+} 

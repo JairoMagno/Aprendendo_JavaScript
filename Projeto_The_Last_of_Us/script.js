b7b0e4c276b81e423cee72b6f7e94f93bca4ab39
Jairo Magno <113738210+JairoMagno@users.noreply.github.com>
2023-03-21 01:05:48 -0300

    Tentando atribuir cor aos botoes

--- Projeto_The_Last_of_Us/script.js
+++ Projeto_The_Last_of_Us/script.js
@@ -4,33 +4,43 @@ let btn_3 = document.getElementById('btn-3')
 let btn_4 = document.getElementById('btn-4')
 let btn_5 = document.getElementById('btn-5')
 let btn_6 = document.getElementById('btn-6')
+const botoes = document.getElementsByClassName('botao')
+
 
 btn_1.addEventListener('click', () => {
     document.body.style.backgroundImage = "url('imagens/tlou-1.jpg')"
-    btn_1.classList.add("selecionado")
 })
 
 btn_2.addEventListener('click', () => {
     document.body.style.backgroundImage = "url('imagens/tlou-2.jpg')"
-    btn_2.classList.add("selecionado")
 })
 
 btn_3.addEventListener('click', () => {
     document.body.style.backgroundImage = "url('imagens/tlou-3.jpg')"
-    btn_3.classList.add("selecionado")
 })
 
 btn_4.addEventListener('click', () => {
     document.body.style.backgroundImage = "url('imagens/tlou-4.jpg')"
-    btn_4.classList.add("selecionado")
 })
 
 btn_5.addEventListener('click', () => {
     document.body.style.backgroundImage = "url('imagens/tlou-5.jpg')"
-    btn_5.classList.add("selecionado")
 })
 
 btn_6.addEventListener('click', () => {
     document.body.style.backgroundImage = "url('imagens/tlou-6.jpg')"
-    btn_6.classList.add("selecionado")
+})
+
+botoes.forEach(botao => {
+    botao.addEventListener('click', function(){
+        //Definir a cor de fundo do botão clicado atual
+        this.style.backGroundColor = '#1d8346'
+
+        //Iterar sobre todos os botões e mudar suas cores
+        for (let i = 0; i< botoes.length; i++){
+            if (botoes[i] !== this) {
+                botoes[i].style.backGroundColor = 'white' //cor padrão
+            }
+        }
+    })
 })
